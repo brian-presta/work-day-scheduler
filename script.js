529f41bda3a9a660d168f58f96788a0eaf99d25b
brian presta <brian.presta@gmail.comgit commit -m first>
2020-06-18 17:41:18 -0700

    added saving to and loading from localstorage, added comments

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-tasks = []
+// dynamically displays date in header
 var writeToday = function(){
     today = moment().format('MMMM Do YYYY')
     $('#currentDay').text(today)
 }
+// colors the backgrounds of the task cells depending on whether they're in the
+// past, present, or future
 var colorCode = function() {
-    var currentTime = moment().format('HA')
-    currentTime = moment(currentTime,'HA')
+    // get current hour as moment object
+    var currentTime = moment().format('h A')
+    currentTime = moment(currentTime,'h A')
+    // iterate over every row
     $(".container").children().each(function(){
+        // get the row's hour as a moment object, get a reference to its sibling task cell
         var cellTime = $(this).find(".hour").text()
-        cellTime = moment(cellTime,"HA")
+        cellTime = moment(cellTime,"h A")
         var description = $(this).find('.description')
+        // remove existing classes, add the currently applicable one
         description.removeClass("past present future")
         if (cellTime.isBefore(currentTime)) {
             description.addClass('past')
@@ -22,28 +28,47 @@ var colorCode = function() {
         }
     })
 };
+// handles editing of task text
 var descriptionClickHandler = function(event) {
+    // replaces the span holding the task text with an text input field
     var span = $(this).find('span')
     var saveButton = $(this).siblings('.saveBtn').find('i')
     var textInput = $("<textarea>").addClass("form-control").val(span.text())
     span.replaceWith(textInput)
     textInput.trigger("focus")
+    // input field persists until user clicks the save button
     saveButton.on('click', function(){
         span.text(textInput.val())
         textInput.replaceWith(span)
         saveTasks()
     })
 };
+// adds task text from every row to localstorage
 var saveTasks = function(){
-    taskArray = []
+    tasks = {}
     $(".description-text").each(function(){
-        taskArray.push($(this).text())
+        key = this.getAttribute("data-key")
+        tasks[key] = $(this).text()
     })
-    tasks = taskArray
+    localStorage.setItem("taskData",JSON.stringify(tasks))
 }
-var onLoad = function(){
-    colorCode()
-    writeToday()
+// pulls task text from localstorage and writes it to the page
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("taskData"))
+    if (!tasks) {
+        // escapes function if there's nothing to load
+        return
+    }
+    $(".description-text").each(function(){
+        key = this.getAttribute("data-key")
+        $(this).text(tasks[key])
+    })
 }
+// fire the on-load functions
+loadTasks()
+colorCode()
+writeToday()
+// listen for clicks on description cells
 $(".description").on('click',descriptionClickHandler)
-onLoad()
+// fires colorCode ever minute to keep cell color up to date
+setInterval(colorCode,(1000*60))
